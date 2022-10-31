84ec8dac406480b7ccea0def3566451c14e97ded
Rene Potocnik <rene.potocnik1@gmail.com>
2022-10-31 21:05:11 +0100

    Initial commit

---
+++ calibrate_positions.py
@@ -0,0 +1,54 @@
+import json
+import time
+import tkinter as tk
+
+
+def calibrate_positions():
+	"""
+	Go though all the buttons and prompt the user to select their location on the screen.
+	Save all the button positions to a JSON file.
+	"""
+	coords: tuple[int, int] = (0, 0)
+
+	def get_mouse_pos(event):
+		root.destroy()
+		nonlocal coords
+		coords = (event.x, event.y)
+	
+	# False for WEB version, True for mobile version
+	mobile_version: bool = False
+
+	positions: dict[str, tuple[int, int]] = {}
+	steps: list[str] = [
+		"take_photo",
+		"send_to",
+		"send_to_input_field",
+		"first_result",
+		"send"
+		]
+
+	print("Starting Calibration\nPrepare to press some buttons!")
+	time.sleep(1)
+
+	for step in steps:
+		# Create the window which will get the mouse positions when clicked
+		root = tk.Tk()
+		root.attributes("-fullscreen", True)
+		root.attributes("-topmost", True)
+		root.attributes("-alpha", 0.1)
+		root.bind("<Button 1>", get_mouse_pos)
+
+		print(f"Current step: '{step}'\tPress the corresponding position on the screen.")
+		root.mainloop()
+		positions[step] = coords
+		print(coords)
+		input("Press enter to continue.\n> ")
+
+	# Write and save the file
+	file_name: str = "ButtonPosition.json"
+	with open(file_name, "w") as pos_file:
+		json.dump(positions, pos_file)
+		print("Positions file has been saved as", file_name)
+
+
+calibrate_positions()
---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+import time
+
+import pyautogui
+
+
+def get_positions(file: str) -> dict[str, list]:
+    try:
+        with open(file, "r") as pos_file:
+            return json.load(pos_file)
+    except FileNotFoundError:
+        exit(f"The file named '{file}' was not found.")
+
+
+def send_snaps(count: int, interval: float, delay: float, positions: dict[str, list], user: str) -> None:
+    """Send snaps on snapchat using mouse movements.
+
+	:param count: How many snaps to send
+	:param interval: How often to send a snap. *{Time to send a snap} + {interval}*
+	:param delay: The delay between each action/step
+	:param positions: Dictionary of all the step positions 
+	:param user: The recepient of the snaps
+	"""
+    for i in range(count):
+        if i:
+            print("Waiting for", interval, "sec.")
+            time.sleep(interval)
+        print(f"Snap #{i + 1}")
+
+        for step, coords in positions.items():
+            print(step)
+            time.sleep(delay)
+
+            pyautogui.click(x=coords[0], y=coords[1])
+            time.sleep(delay)
+
+            if step == "send_to_input_field":
+                pyautogui.write(user)
+
+
+def main() -> None:
+    # Get the position of all the steps on screen from a JSON file
+    step_positions: dict[str, list] = get_positions(file="ButtonPosition.json")
+
+    count = int(input("Amount of snaps to send (default: 10): "))
+    interval = float(input("Time between each snap (default: 3): "))
+    delay = float(input("Delay between actions (default: 0.4): "))
+    user = input(f"Recipient: ")
+
+    send_snaps(count=count if count else 10,
+               interval=interval if interval else 3,
+               delay=delay if delay else 0.4,
+               positions=step_positions,
+               user=user)
+
+
+if __name__ == '__main__':
+    main()
+    exit("Done!")
